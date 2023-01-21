ca919704ecac8e14e4cb07a88c32b798a71a3c91
BryonnaC <77491054+BryonnaC@users.noreply.github.com>
2023-01-20 20:57:43 -0500

    extra credit

--- src/lab2/map_cell.py
+++ src/lab2/map_cell.py
@@ -23,3 +23,35 @@ Your task is to write a function that can translate from strings of length 2 to 
 
 For example, calling get_row_col("A3") should return the tuple (2, 0) because A3 corresponds to the row at index 2 and column at index 0in the board.
 '''
+
+'''
+A1  B1  C1
+A2  B2  C2
+A3  B3  C3
+'''
+
+def get_row_col(input_string):
+    switch = {
+        "A1" : (0,0),
+        "A2" : (1,0),
+        "A3" : (2,0),
+        "B1" : (0,1),
+        "B2" : (1,1),
+        "B3" : (2,1),
+        "C1" : (0,2),
+        "C2" : (1,2),
+        "C3" : (2,2)
+    }
+
+    return switch.get(input_string, "Not a valid position")
+
+if __name__ == '__main__':
+    square = "C1"
+    invalid_square = "C4"
+    
+    print(get_row_col(square))
+    print(get_row_col(invalid_square))
+
+
+
+
