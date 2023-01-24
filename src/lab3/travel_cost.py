4cc14ca7ff1198280b2dca5dab3ad6ab6582e3cf
BryonnaC <77491054+BryonnaC@users.noreply.github.com>
2023-01-24 15:15:52 -0500

    bringing over changes from azure vscode

--- src/lab3/travel_cost.py
+++ src/lab3/travel_cost.py
@@ -39,7 +39,101 @@ def get_route_cost(route_coordinate, game_map):
     :return: a floating point number representing the cost of the route
     """
     # Build a path from start to end that looks like [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4), (5, 4)]
-    pass 
+
+    # Get the two cities coordinates
+    city_one = route_coordinate[0]
+    city_two = route_coordinate[1]
+
+    # Split the tuples into individual coordinates 
+    city_one_x = city_one[0]
+    city_one_y = city_one[1]
+    city_two_x = city_two[0]
+    city_two_y = city_two[1]
+    
+    # We want to go diagonal in the X direction according to lab instructions
+    # So compare the x's of city one and city two
+    x_diff = city_two_x - city_one_x
+    # but now we also need to check y so that we know if we're going up or down
+    y_diff = city_one_y - city_two_y
+    # TODO consider negative vs positive values (left v right)
+
+    # Create the new list - looks like it should be named path
+    path = [ ]
+    # x_diff is the number of times that the player needs to move diagonal
+    global working_y  # might not need global? unsure, can test
+    global working_x  # just need it to hold outside of for loop
+
+    working_x = city_one_x
+    working_y = city_one_y
+
+    # Check whether |y_diff| >= |x_diff|
+    y_abs = 0
+    x_abs = 0
+
+    if(y_diff > 0):
+      y_abs = y_diff
+    if(y_diff < 0):
+      y_abs = -y_diff
+    if(x_diff > 0):
+      x_abs = x_diff
+    if(x_diff < 0):
+      x_abs = -x_diff
+
+    # What if x_diff is equal to zero
+    if(x_diff == 0):
+      diag_moves = 0 # right? but does this case matter? yes - it should probably get checked before all others
+    else:
+      if( y_abs < x_abs):
+        # Need the different of y_abs and x_abs
+        diag_moves = x_abs - y_abs
+      # Otherwise, there is equal or more movement in the y dir as is in x dir
+      if(y_abs >= x_abs):
+        diag_moves = x_diff
+      # Here we're finally figuring out how many diagonals to do
+      for i in range(1, diag_moves-1): # Change*d* this to diag_moves, but does that mess with my neg/pos? probably
+        if(x_diff > 0):              # no, actually, I think  if I keep these if compares the same, it'll probably work out.
+          working_x += 1
+        if(x_diff < 0):
+          working_x -= 1
+        if(y_diff > 0):
+          working_y += 1
+        if(y_diff < 0):
+          working_y -= 1
+        # Now add the tuple to the list
+        path_item = (working_x,working_y)
+        path.append(path_item)
+
+    # Now we need to figure out if there's any more movements (x or y)
+    remain_moves = 0
+    if(x_abs > y_abs):
+      # We need to do this many more x movements
+      remain_moves = x_abs - y_abs
+    if(y_abs > x_abs):
+      # We need to do this many more y movements
+      remain_moves = y_abs - x_abs
+
+    # TODO make the for loop into a function, seems to be reused quite often
+    for j in range(1, remain_moves-1):
+      # TODO this is not optimal
+      if(x_abs > y_abs):
+        if(x_diff > 0):              
+          working_x += 1
+        if(x_diff < 0):
+          working_x -= 1
+        # Combine with last known working_y
+        path_item = (working_x,working_y)
+        path.append(path_item)        # TODO ultra repetitive code 
+      
+      if(y_abs > x_abs):
+        if(y_diff > 0):
+          working_y += 1
+        if(y_diff < 0):
+          working_y -= 1
+        # Combine with last known working_x
+        path_item = (working_x,working_y)
+        path.append(path_item)        # TODO ultra repetitive code 
+
+    #pass 
     return game_map[tuple(zip(*path))].sum()
 
 
