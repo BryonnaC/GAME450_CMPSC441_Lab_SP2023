afad027a94ebfbbcf6f3ef05c3763e39b8b681b0
BryonnaC <77491054+BryonnaC@users.noreply.github.com>
2023-03-26 19:20:08 -0400

    need to sync branch

--- src/lab11/pygame_ai_player.py
+++ src/lab11/pygame_ai_player.py
@@ -1,12 +1,30 @@
+import pygame
+from lab11.turn_combat import CombatPlayer
 """ Create PyGameAIPlayer class here"""
 
 
 class PyGameAIPlayer:
+    def __init__(self) -> None:
+        pass
+
+    def selectAction(self, state):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if ord("0") <= event.key <= ord("9"):
+                    return event.key
+        return ord(str(state.current_city))  # Not a safe operation for >10 cities
+
     pass
 
 
 """ Create PyGameAICombatPlayer class here"""
 
 
-class PyGameAICombatPlayer:
+class PyGameAICombatPlayer(CombatPlayer):
+    def __init__(self, name):
+        super().__init__(name)
+
+    
     pass
